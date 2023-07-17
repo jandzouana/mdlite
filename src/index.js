ac3ecfe36bd841b11bbedaed814216b78251337c
jandzouana <jandzouana@alcacruz.com>
2023-07-17 14:20:19 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+//
+// const app = document.getElementById('app');
+// const root = ReactDOM.createRoot(app);
+// root.render(<App/>);
+
+// For react 17.0.0
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+// TODO: App strict mode?
+ReactDOM.render(<App />, document.getElementById("app"))
