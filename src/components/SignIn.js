9750e18b10178e81776b02ffdfdd1e4b52e3b253
jandzouana <jandzouana@alcacruz.com>
2023-07-19 13:31:21 -0700

    Fixed signin loader not working as intended, added google logo to sign in button

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,25 +1,31 @@
 import React, {useState, useEffect, useRef} from 'react';
 import { auth, provider, popup, app, persistence } from '../firebase';
 import logo from '../assets/logo.png';
+import glogo from '../assets/glogo.png';
 
 const SignIn = (props) =>{
     const [showLoader, setShowLoader] = useState(false);
-    const popupShowing = useRef(false);
+    const popupClickCount = useRef(0);
 
     function handleClick(){
-        popupShowing.current = true;
+        popupClickCount.current = popupClickCount.current + 1;
         setShowLoader(true);
         const remember = document.getElementById("remember");
         console.log("Setting persistence: " + remember.checked);
-
         popup(auth, provider).then(data => {
+            console.log("data", data);
+
             auth.setPersistence(remember ? persistence : null).then(r => {
 
             });
         }).catch(exception => {
             console.log("Request didn't go through:\n" + exception);
-            if(!popupShowing) setShowLoader(false);
-            popupShowing.current = false;
+            setTimeout(()=>{
+                if(popupClickCount.current <= 1) {
+                    setShowLoader(false);
+                }
+                else popupClickCount.current = 0;
+            }, 500)
         })
     }
 
@@ -27,7 +33,7 @@ const SignIn = (props) =>{
         <div id={"sign-in-container"}>
             <img id="sign-in-logo" src={logo} alt="logo" />
             <section>
-                <button id={"sign-in"} className={"button"} onClick={handleClick}>Sign in With Google</button>
+                <button id={"sign-in"} className={"button purple-button"} onClick={handleClick}>Sign in With Google<img src={glogo} alt={"glogo"}/></button>
                 <section>
                     <input type={"checkbox"} name={"remember"} id={"remember"}/>
                     <label htmlFor={"remember"}>Remember me</label>
